1f6069cafb429d2f37fb7db299d7b63dea8356d0
Ros <magali.ros@KC1ROSMAGALIMAC.dsiglobal.com>
2018-10-17 11:19:54 -0500

    Fix responsiveness

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -164,10 +164,12 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
 createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
+  name.className = "reviews-name";
   name.innerHTML = review.name;
   li.appendChild(name);
 
   const date = document.createElement('p');
+  date.className = "reviews-date";
   date.innerHTML = review.date;
   li.appendChild(date);
 
